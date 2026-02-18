56e2dd10355c0f2540105e4cb9128f984e902f86
Mikkel <mikkelmb123@gmail.com>
2026-02-18 10:18:44 +0100

    threads

--- exercises/multi/main.cc
+++ exercises/multi/main.cc
@@ -1,13 +1,39 @@
+#include<thread>
+#include<string>
 #include<iostream>
+#include<vector>
 
-struct data { int a,b; double sum;};
-void harm(data& arg){
-	int a=arg.a,b=arg.b; // you better work
-	double sum=0;        // with thread-local variables
-	for(int i=a;i<b;i++)sum+=1.0/i;
-	arg.sum=sum; // only write once into memory
-	}
+struct datum {long long start,end; double sum;};
+void harm(datum& arg){
+    long long start = arg.start, end=arg.end;
+    double sum = 0;
+    for(long long i=start; i<end; i++) sum += 1.0/(double)i;
+    arg.sum = sum;
+}
+
 
 int main(int argc, char** argv) {
+    long long nterms = 1000000000000000; int nthreads=1;
+    for(int i=0;i<argc;i++) {
+        std::string arg = argv[i];
+        if(arg=="-terms" && i+1 < argc) nterms=(int)std::stoll(argv[++i]);
+        if(arg=="-threads" && i+1 < argc) nthreads=(int)std::stoi(argv[++i]);
+    }
+    std::cout << "terms: " << nterms << std::endl;
+    std::cout << "threads: " << nthreads << std::endl;
+
+    std::vector<std::thread> threads;
+    threads.reserve(nthreads);
+    std::vector<datum> data(nthreads);
+    for(int i=0;i<nthreads;++i){
+        data[i].start=1+(nterms/nthreads)*i;
+        data[i].end=1+(nterms/nthreads)*(i+1);
+        threads.emplace_back(harm,std::ref(data[i]));
+    }
+    for(std::thread &thread : threads)thread.join();
+    double total =0;
+    for(datum &d : data)total += d.sum;
+    std::cout << "Total sum = " << total << std::endl;
     
+
 return 0;}
