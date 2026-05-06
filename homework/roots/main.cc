ce1ae4bb5170fd73eaa7f9045a4c544f9eb6ce0d
Mikkel <mikkelmb123@gmail.com>
2026-05-06 08:29:19 +0200

    roots A

---
+++ homework/roots/main.cc
@@ -0,0 +1,45 @@
+#include "newton.h"
+#include "matrix.h"
+
+using namespace pp;
+
+int main(){
+
+    // --- Part A ---
+    auto R_dfdx = [](vector x) -> double {
+        return -2.0*(1.0-x[0]) + 200.0*(x[1]-x[0]*x[0]) * (-2.0*x[0]);
+    };
+    auto R_dfdy = [](vector x) -> double {
+        return 200.0*(x[1]-x[0]*x[0]);
+    };
+
+    Func_list fl_1 = {R_dfdx, R_dfdy};
+    vector point = {0.5, 0.5};
+    vector root1 = newton(fl_1, point);
+
+    std::cout << "Roots of Rosenbrock gradient: " << root1 << "\n"
+              << "Exact: (1,1)" << "\n" ;
+
+
+    auto H_dfdx = [](vector x) -> double {
+        return 4.0*x[0]*(x[0]*x[0]+x[1]-11.0)
+               + 2.0*(x[0]+x[1]*x[1]-7.0);
+    };
+    auto H_dfdy = [](vector x) -> double {
+        return 2.0*(x[0]*x[0]+x[1]-11.0) 
+               + 4.0*x[1]*(x[0]+x[1]*x[1]-7.0);
+    };
+
+    Func_list fl_2 = {H_dfdx, H_dfdy};
+    matrix points = {{-2.0, -2.0}, {-3.0, 3.0}, {-3.0, -3.0}, {3.0, -2.0}};
+
+    std::cout << "Roots of Himmelblau gradient: " << "\n";
+    for(size_t i=0; i<4; i++){
+        std::cout << "Starting point: " << points[i] 
+                  << " Result: " << newton(fl_2, points[i]) << "\n";
+    }
+    std::cout << "Minima: (3.0, 2.0), (-2.805118, 3.131312), (-3.779310, -3.283186), (3.584428, -1.848126) \n"
+              << "Maxima: (-0.270845, -0.923039) \n";
+
+    return 0;
+}
---
+++ homework/roots/newton.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include "matrix.h"
+#include "QR.h"
+
+namespace pp {
+
+using Func = std::function<double(vector)>;
+using Func_list = std::vector<Func>;
+
+// Helper to evaluate the list of functions f at point x
+vector eval(const Func_list& flist, const vector& x) {
+    vector res(flist.size());
+    for(size_t i = 0; i < flist.size(); ++i) {
+        res[i] = flist[i](x);
+    }
+    return res;
+}
+
+
+matrix jacobian(Func_list f, vector x, vector fx) {
+    size_t dim = x.size();
+    matrix J(dim, dim);
+
+    for(size_t i=0; i<dim; i++) {
+        double dx = std::abs(x[i])*std::pow(2,-26);
+        //if(dx < ZERO_LIMIT) dx = ZERO_LIMIT;
+        x[i] += dx;
+        vector df = eval(f, x)-fx;
+        for(size_t j=0; j<dim; j++){
+            J(j,i) = df[j]/dx;
+        }
+        x[i] -= dx;
+    }
+
+    return J;
+}
+
+vector newton (Func_list f, vector x, double acc = 1e-4, double alpha_min = 1e-3, int max_iter = 1000){
+    vector fx = eval(f, x); 
+    for(int i=0; i<max_iter; i++){
+        if (fx.norm() < acc) break;
+        matrix J = jacobian(f, x, fx);
+        QR QRJ(J);
+        vector Dx = QRJ.solve(-fx);
+        double alpha = 1.0;
+
+        vector z;
+        vector fz;
+        while(true) {
+            z = x + alpha * Dx;
+            fz = eval(f, z);
+            // std::cerr << fx.norm() << "\n"; 
+            if(fz.norm() < fx.norm()) break;
+            if(alpha < alpha_min) {std::cerr << "failed\n"; break;}
+            alpha /= 2.0; 
+        }
+
+        x = z; 
+        fx = fz;
+    }
+
+    return x;
+}
+
+} // namespace pp
